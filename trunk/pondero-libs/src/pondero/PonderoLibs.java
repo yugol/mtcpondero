c68c0369082eb8d03a3c36708e70066ad97cbe13
mindtrips.communications@gmail.com <mindtrips.communications@gmail.com@63905f44-0079-8ae6-1c7d-32471a5c9c6f>
2014-06-22 06:02:40 +0000

    pondero-libs: initial import

---
+++ trunk/pondero-libs/src/pondero/PonderoLibs.java
@@ -0,0 +1,17 @@
+package pondero;
+
+public class PonderoLibs {
+
+    public static void main(String[] args) {
+        System.out.println("This is the Pondero application dependencies project");
+    }
+
+    public int getMajorVersion() {
+        return 1;
+    }
+
+    public int getMinorVersion() {
+        return 0;
+    }
+
+}
